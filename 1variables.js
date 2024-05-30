a7bc703da51b4fde871b4b0e998602eb4ec7fec0
xyfer34 <dreambeast890@gmail.com>
2024-05-30 10:44:23 +0000

    done

---
+++ 1variables.js
@@ -0,0 +1,7 @@
+const accountId = "123123"
+let accountEmail = "pik@gmail.com"
+var accountPassword = "123123123"
+
+accountEmail = "pika@gmai.com"
+accountPassword = "1451454"
+console.table([accountId, accountEmail, accountPassword])
